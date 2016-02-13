ea555a7cd8841e659973281c2a74aae07c48ca17
morganowen <mog13@live.com>
2016-02-13 22:27:16 +0000

    Added Karma testing framework and its configuration. Included Gulp tasks to build a minified library and to run unit tests.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,25 @@
-/**
- * Created by morganowen on 13/02/16.
- */
+
 var gulp = require('gulp');
+var Server = require('karma').Server;
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 gulp.task('default', function() {
     // place code for your default task here
+});
+
+//Run all Jasmine unit tests (using Karma)
+gulp.task('unit-tests', function (done) {
+    new Server({
+        configFile: __dirname + '/karma.conf.js',
+        singleRun: true
+    }, done).start();
+});
+
+
+gulp.task('minify', function() {
+    return gulp.src('js/*.js')
+        .pipe(concat('dx.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/'));
 });
---
+++ tests/test.js
@@ -0,0 +1,9 @@
+/**
+ * Created by morganowen on 13/02/16.
+ */
+describe('',function(){
+
+    it('',function(){
+        expect(0).toBeGreaterThan(1);
+    })
+});
