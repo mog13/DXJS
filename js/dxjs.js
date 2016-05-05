e8a8220f9ca4dbc8884e23c1364489ca6e31252c
mog13 <mog13@live.com>
2016-05-06 00:20:35 +0100

    Added in arbritrary roll function execution

--- js/dxjs.js
+++ js/dxjs.js
@@ -23,53 +23,83 @@ dx.prototype.showDice = function(){
 }
 
 dx.prototype._tryToParseDice = function(candidate){
+  console.log('trying to create dice: ',candidate);
   if(/[^1-9]{0}d[1-9]+/.test(candidate)){
-      var newDice = /[^1-9]{0}d[1-9]+/.exec(candidate);
+      var newDice = /[^1-9]{0}d[1-9]+/.exec(candidate)[0];
       var temp = parseInt(newDice.replace(/[a-z A-Z]/g,''));
       var faces = [];
       for(var n = 1; n <= temp; n++){
-        face.push(n);
+        faces.push(n);
       }
       this[newDice] = new dice(candidate,faces);
   }
   else{
-    throw('no such dice: ' +rollFunc)
+    throw('no such dice: ' +candidate)
   }
 };
 
 dx.prototype._evaluateSingleRoll = function(rollFunc) {
   var rolls = [];
-var index = /[a-z]/.exec(rollFunc).index;
-var repeater = 1;
-if(index > 0) repeater = parseInt(rollFunc.substring(0,index));
-if(index >=0){
-  var die = rollFunc.substring(index);
-  //if it doesnt exist try and make one (in form of dX)
-  if(this[die] === undefined){
-    this._tryToParseDice();
-    //@todo need better catching here!
+  var index = /[a-z]/.exec(rollFunc).index;
+  var repeater = 1;
+  if(index > 0) repeater = parseInt(rollFunc.substring(0,index));
+  if(index >=0){
+    var die = rollFunc.substring(index);
+    //if it doesnt exist try and make one (in form of dX)
+    if(this[die] === undefined){
+      this._tryToParseDice(die);
+      //@todo need better catching here!
+    }
+  var value = 0;
+    for(var i =0; i <repeater;i++){
+      var roll = this[die].roll();
+      rolls.push({'value':roll,'throw':die});
+      value += roll;
+    }
+
   }
-var value = 0;
-  for(var i =0; i <repeater;i++){
-    var roll = this[die].roll();
-    rolls.push({'value':roll,'throw':die});
-    value += roll;
+  return {
+    'value':value,
+    'throw':rollFunc,
+    'rolls':rolls
+  };
+};
+
+dx.prototype.roll = function(rollFunc) {
+
+  //spiit dice roll by operators
+  var parts = rollFunc.split(/([\+\-\/\*])/);
+  var rolls = [];
+  //for each one evaluate it as a single roll
+  var op = undefined;
+  var value = 0;
+  var that = this;
+  parts.forEach(function(roll){
+  switch(roll) {
+    case '+':
+    case '-':
+    case '/':
+    case '*':
+      op = roll;
+      break;
+    default:
+      var rollResult = that._evaluateSingleRoll(roll.trim())
+      rolls.push(rollResult);
+      if(op === '+' | op === undefined) value += rollResult.value;
+      else if(op === '-') value -= rollResult.value;
+      else if(op === '*') value *= rollResult.value;
+      else if(op === '/') value /= rollResult.value;
   }
 
-}
-
-return {
-  'value':value,
-  'throw':rollFunc,
-  'rolls':rolls
+});
+  return {
+    'value':value,
+    'throw':rollFunc,
+    'rolls':rolls
+    };
 };
-};
 
 
-dx.prototype.evaluateRoll = function(rollFunc) {
-  var funcOut =  {};
-
-}
 
 DX = new dx();
 
